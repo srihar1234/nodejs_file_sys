89fb57be0c9f60b7a3765c8f7f9f1ac329eb755d
HARI <143261710+srihar1234@users.noreply.github.com>
2023-12-14 21:38:37 +0530

    slight change

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 const timestamp = require('time-stamp');
 
 
-app.get("/create",(req,res)=>{
+app.get("/",(req,res)=>{
        fs.writeFileSync(`./timeStamps/${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt`,`${timestamp.utc('YYYY/MM/DD:HH:mm:ss')}`);
-        res.send(`${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt file is created in timeStamps folder with date-time as its content`);
+        res.send(`${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt file is created in timeStamps folder with date-time as its content.\n
+                  To read files created till now on timestamps folder,use API endpoint"/read"`);
 });
 
 
