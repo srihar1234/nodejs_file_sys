59efcae06176d517f1d6171352fff479efa7df05
HARI <143261710+srihar1234@users.noreply.github.com>
2023-12-14 21:49:09 +0530

    3rd endpoint

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ app.use(express.json());
 const fs = require("fs");
 const timestamp = require('time-stamp');
 
-
 app.get("/",(req,res)=>{
+    res.send("You have reached a file system zen task to create Api endpoints.\n1)API endpoint to create file==>`/create`\n2)API to retrive files==>`/read`")
+})
+app.get("/create",(req,res)=>{
        fs.writeFileSync(`./timeStamps/${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt`,`${timestamp.utc('YYYY/MM/DD:HH:mm:ss')}`);
-        res.send(`${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt file is created in timeStamps folder with date-time as its content.\n
-                  To read files created till now on timestamps folder,use API endpoint"/read"`);
+        res.send(`${timestamp.utc('YYYY-MM-DD-HH-mm-ss')}.txt file is created in timeStamps folder with date-time as its content.\nTo read files created till now on timestamps folder,use API endpoint"/read"`);
 });
 
 
